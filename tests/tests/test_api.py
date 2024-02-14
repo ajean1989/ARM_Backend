9417492d0fe459a41bf141f5bd162f0ade545c01
ajean1989 <105272808+ajean1989@users.noreply.github.com>
2024-02-14 08:57:57 +0100

    experiments pipeline ci/cd

--- tests/tests/test_api.py
+++ tests/tests/test_api.py
@@ -2,7 +2,6 @@ import httpx
 from config import *
 # Tests des différentes ressources
 
-api = "api-backend"
 
 # API KEY
 
@@ -10,9 +9,9 @@ headers = {'X-API-Key': list(API_KEYS.keys())[0]}
 
 
 def test_helloworld_raw() :
-    response = httpx.get(f"http://{api}:6001/", headers = headers)
+    response = httpx.get(f"http://api-backend:6001/", headers = headers)
     assert response.status_code == 200
 
 def test_helloworld_traefik() :
-    response = httpx.get(f"http://{api}/api-backend/", headers = headers)
+    response = httpx.get(f"http://traefik/api-backend/", headers = headers)
     assert response.status_code == 200
