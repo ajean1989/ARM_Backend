94a073de78892438af3eeef5d27ce516695fad50
adrien <adrien.jean1989@gmail.com>
2024-05-08 15:36:34 +0200

    split docker compose dev prod + HTTPS

--- tests/tests/test_api.py
+++ tests/tests/test_api.py
@@ -12,9 +12,9 @@ headers = {'X-API-Key': list(API_KEYS.keys())[0]}
 #     response = httpx.get(f"http://api-backend:6001/", headers = headers)
 #     assert response.status_code == 200
 
-def test_adminer_traefik() :
-    response = httpx.get(f"https://traefik/adminer/", headers = headers, verify=False)
-    assert response.status_code == 200
+# def test_adminer_traefik() :
+#     response = httpx.get(f"https://traefik/adminer/", headers = headers, verify=False)
+#     assert response.status_code == 200
 
 def test_helloworld_traefik() :
     response = httpx.get(f"https://traefik/api-backend/", headers = headers, verify=False)
